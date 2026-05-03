9dd2ae5026ed8f7732729ebc08cc926a6bc54e65
Mauricio Nero <eu@mauricionero.com>
2026-05-03 20:07:38 -0300

    1.10.1: better monitor overlay with table or grid view

--- frontend/src/config/version.js
+++ frontend/src/config/version.js
@@ -3,7 +3,7 @@
 // This file is imported by both the Setup and Overlay pages to display the
 // version number in the UI.
 
-export const VERSION = '1.11.0';
+export const VERSION = '1.11.1';
 export const APK_VERSION = '1.2';
 export const APK_FILE_NAME = `nrs-control-zone-${APK_VERSION}.apk`;
 
