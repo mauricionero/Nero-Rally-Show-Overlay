3021a98728cabd2fda5384f778944c463ceff623
Mauricio Nero <eu@mauricionero.com>
2026-05-07 16:11:41 -0300

    1.12.0: pilots page improved

--- frontend/src/config/version.js
+++ frontend/src/config/version.js
@@ -3,7 +3,7 @@
 // This file is imported by both the Setup and Overlay pages to display the
 // version number in the UI.
 
-export const VERSION = '1.11.3';
+export const VERSION = '1.12.0';
 export const APK_VERSION = '1.2';
 export const APK_FILE_NAME = `nrs-control-zone-${APK_VERSION}.apk`;
 
